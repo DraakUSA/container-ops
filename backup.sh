4512df70478082d07d3a823260f1b1c5685d5b63
Paul-Joseph de Werk <dewerk@gmail.com>
2026-01-18 15:33:08 -0800

    Stop tracking sensitive/local files while keeping them on disk

--- backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp /home/dockeruser/docker/networking/netalertx/data/config/app.conf /home/dockeruser/backups/netalertx/app_conf_$(date +%Y%m%d_%H%M).bak
-cp /home/dockeruser/docker/networking/netalertx/data/db/app.db /home/dockeruser/backups/netalertx/app_$(date +%Y%m%d_%H%M).db
